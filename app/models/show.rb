a5b6ceb04dc1082320e8ac0d6ab078e5d33beed5
Zachary Dean <Zakedoubled@gmail.com>
2022-09-06 13:34:04 -0400

    commit tree 44af8ec479db40ee26d23973545483db97bc55f6

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors_list = []
+    self.characters.each do |character|
+        actors_list << character.actor.full_name
+    end
+    actors_list
+  end
 end
